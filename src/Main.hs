5053d26121706c8321958368b12f898cb8f37a1b
Jonas Juselius <jonas.juselius@uit.no>
2014-08-24 22:17:02 +0200

    Rudimentary auditlog/ldif parsing.

--- src/Main.hs
+++ src/Main.hs
@@ -1,34 +1,52 @@
-{-# OPTIONS_GHC -funbox-strict-fields #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import System.IO
 import Control.Concurrent
-{-import Text.Regex.PCRE-}
+import Text.LDIF.Parser
+import qualified Data.ByteString.Char8 as BS
+import Text.Regex.Posix
 import Settings
 
 main = do
     settings <- readSettings "ldaprelay.cfg"
-    print settings
-    inh <- openFile "input.txt" ReadMode
+    inh <- openFile (auditlog settings) ReadMode
     processData inh
     hClose inh
-    putStrLn "done."
+    BS.putStrLn "done."
 
-waitForLine :: Handle -> IO String
+waitForLine :: Handle -> IO BS.ByteString
 waitForLine inh = do
     atEof <- hIsEOF inh
     if atEof
         then do
             threadDelay 50000
             waitForLine inh
-        else hGetLine inh
+        else BS.hGetLine inh
+
+isEmptyLine :: BS.ByteString -> Bool
+isEmptyLine line =
+    line =~ BS.pack "^ *$" :: Bool
+
+isBeginAuditLog :: BS.ByteString -> Bool
+isBeginAuditLog line =
+    line =~ BS.pack "^# (add|modify|delete) ([0-9]+)" :: Bool
+
+readAuditlogBlock :: Handle -> [BS.ByteString] -> IO BS.ByteString
+readAuditlogBlock inh acc = do
+    line <- waitForLine inh
+    if isEmptyLine line || isBeginAuditLog line
+        then readAuditlogBlock inh acc
+        else if line =~
+            BS.pack "^# end (add|modify|delete) ([0-9]+)" :: Bool
+            then return $ BS.unlines acc
+            else readAuditlogBlock inh (acc ++ [line])
 
 processData :: Handle ->  IO ()
 processData inh = do
-    str <- waitForLine inh
-    putStrLn str
+    ldata <- readAuditlogBlock inh []
+    case parseLDIFStr defaulLDIFConf "relay" ldata of
+        Left err -> print "err"
+        Right ldif -> print ldif
     processData inh
 

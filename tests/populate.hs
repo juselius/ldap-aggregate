77d8f4a85d3c6abe8ce5a8db2ad348e12e8f73f6
Jonas Juselius <jonas.juselius@uit.no>
2014-08-27 23:52:56 +0200

    Improvements.

--- tests/populate.hs
+++ tests/populate.hs
@@ -8,35 +8,39 @@ import LDIF.Simple
 import Text.Regex.Posix
 import qualified Data.ByteString.Char8 as BS
 
-import Debug.Trace
-import Text.LDIF.Preproc
-
 main :: IO ()
 main = do
     populateSource
 
 populateSource :: IO ()
 populateSource = do
+    ltree <- withFile "./ldif/source.tree.ldif" ReadMode BS.hGetContents
+    lpops <- withFile "./ldif/source.populate.ldif" ReadMode BS.hGetContents
     source <- bindDIT "dc=source"
+    let commit x = mapM_ (\(dn, attrs) ->
+            ldapAdd source dn attrs) (ldata2LdapMod x)
+    print $ ldata2LdapMod ltree
+    --commit ltree
+    --commit lpops
     printDIT source "dc=source"
-    putStrLn ""
-    ldata <- withFile "./ldif/source.populate.ldif" ReadMode BS.hGetContents
-    let ldif = extractEntries $ parseLDIFStr "" ldata
-        addldif = map (\(LDAPEntry dn attrs) ->
-            (dn, list2ldm LdapModAdd attrs)) ldif
-    print ldif
-    mapM_ (\(dn, attrs) -> ldapAdd source dn attrs) addldif
-    --printDIT source "dc=source"
 
     --ldapModify ldap "cn=admin,dc=source" [chAdminDesc "Head Foo"]
     --ldif' <- search Nothing LDAPAllUserAttrs False
     --print ldif'
     --where
         --chAdminDesc x = LDAPMod LdapModReplace "description" [x, x ++ "bar"]
+
+ldata2LdapMod :: BS.ByteString -> [(String, [LDAPMod])]
+ldata2LdapMod str =
+    case ldif of
+        Right l -> map (\(LDAPEntry dn attrs) ->
+            (dn, list2ldm LdapModAdd attrs)) l
+        Left e -> error e
     where
+        ldif = extractEntries $ parseLDIFStr "" str
         extractEntries x = case x of
-            Right l -> map (\z -> entry z) l
-            Left err -> error $ show err
+            Right l -> Right $ map (\z -> entry z) l
+            Left err -> Left $ show err
 
 printDIT :: LDAP -> String -> IO ()
 printDIT ldap tree = do
@@ -47,7 +51,7 @@ printDIT ldap tree = do
 bindDIT :: String -> IO LDAP
 bindDIT tree = do
     --pw <- getBindPw
-    ldap <- ldapInitialize "ldap://localhost"
+    ldap <- ldapInitialize "ldap://localhost:1389"
     ldapSimpleBind ldap ("cn=admin," ++ tree) "secret"
     return ldap
 

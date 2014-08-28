5577ade29e9254860dd4a1f7ee28b44bef9a25b7
Jonas Juselius <jonas.juselius@uit.no>
2014-08-28 12:45:34 +0200

    Renamed tests.

--- tests/setup-tree.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -n "bindpw: "
-read pw
-
-ldapadd -H ldap://localhost -D cn=admin,dc=source -w $pw\
-    -f ./ldif/source.create.ldif
-ldapadd -H ldap://localhost -D cn=admin,dc=target -w $pw\
-    -f ./ldif/target.create.ldif

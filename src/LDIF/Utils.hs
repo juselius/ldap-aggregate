7dc33415e3b1c6c538ff628a837a9fc4fc8bac3d
Jonas Juselius <jonas.juselius@uit.no>
2014-09-02 15:37:09 +0200

    Added LDIF manipulation utilities.

---
+++ src/LDIF/Utils.hs
@@ -0,0 +1,31 @@
+{-| Helpers for working with Simple LDIF
+
+    <jonas.juselius@uit.no> 2014
+-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module LDIF.Utils (
+      toAssocList
+    , toHashMap
+    , isUniqDN
+) where
+
+import LDIF.Simple
+import Data.List
+import Data.Maybe
+import qualified Data.HashMap.Lazy as HM
+
+toAssocList :: [LDIF] -> [(DN, LDIFRecord)]
+toAssocList = map ldifEntry
+
+toHashMap :: [LDIF] -> HM.HashMap DN LDIFRecord
+toHashMap xs = HM.fromList $ map ldifEntry xs
+
+isUniqDN :: [LDIF] -> Bool
+isUniqDN xs = case hasDuplicates $ toAssocList xs of
+        Just _ -> False
+        Nothing -> True
+    where
+        hasDuplicates x = listToMaybe $
+            nubBy (\(dn, _) (dn', _) -> dn == dn') x
+

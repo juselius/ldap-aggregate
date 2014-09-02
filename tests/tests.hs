8660391c8d7b33c228e48e3f129d8c44242c1236
Jonas Juselius <jonas.juselius@uit.no>
2014-09-02 08:33:11 +0200

    Added stub Tasty testing framework.

--- tests/tests.hs
+++ tests/tests.hs
@@ -1,10 +1,31 @@
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+
 import Data.Char
-import Test.QuickCheck
 
-main = do
-    xmpl
-    xmpl'
+main = defaultMain tests
 
-xmpl = quickCheck ((\s -> (reverse.reverse) s == s) :: [Char] -> Bool)
+tests :: TestTree
+tests = testGroup "Tests" [properties, unitTests]
 
-xmpl' = verboseCheck ((\s -> length s < 3) :: [Integer] -> Bool)
+properties :: TestTree
+properties = testGroup "Properties"
+    [ QC.testProperty "example 1" xmpl
+    , QC.testProperty "example 2" xmpl'
+    ]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+  [ testCase "List comparison (different length)" $
+      [1, 2, 3] `compare` [1,2] @?= GT
+
+  -- the following test does not hold
+  , testCase "List comparison (same length)" $
+      [1, 2, 3] `compare` [1,2,2] @?= LT
+  ]
+
+xmpl = ((\s -> (reverse.reverse) s == s) :: [Char] -> Bool)
+
+xmpl' = ((\s -> length s < 3) :: [Integer] -> Bool)
+

e8ccb1170d1d31f7661d692aa723f4a96e5a0186
Jonas Juselius <jonas.juselius@uit.no>
2014-09-03 14:49:20 +0200

    Stub comparison and filtering functions.

--- src/LDIF/Compare.hs
+++ src/LDIF/Compare.hs
@@ -2,29 +2,45 @@
 
     <jonas.juselius@uit.no> 2014
 -}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, LiberalTypeSynonyms #-}
 
 module LDIF.Compare (
       filterEqDN
+    , filterEq
+    , filterAttrs
 ) where
 
 import LDIF.Simple
 import LDIF.Utils
+import Data.List
 import Data.Maybe
 
 filterEqDN :: [LDIF] -> [LDIF] -> [LDIF]
-filterEqDN src dest =
-    map LDIF filterMatching
-    where
-        filterMatching = filter (\x -> isNothing $ lookup (fst x) dest') src'
-        src' = toAssocList src
-        dest' = toAssocList dest
-
+filterEqDN from to =
+    map LDIF $ filter (isNothing . flip lookup (toAL to) . fst) (toAL from)
 
 filterEq :: [LDIF] -> [LDIF] -> [LDIF]
-filterEq src dest =
-    map LDIF filterMatching
+filterEq from to =
+    map LDIF $ filter (\(dn, rec) ->
+        maybe False (== rec) $ lookup dn (toAL to)) (toAL from)
+
+filterAttrs :: (Eq a, Eq b) => [(a, [b])] -> [(a, [b])] -> [(a, [b])]
+filterAttrs from to = missing from to ++ diffs
     where
-        filterMatching = filter (\x -> case lookup (fst x) dest') src'
-        src' = toAssocList src
-        dest' = toAssocList dest
+        differ = filter (\(a, v) ->
+            v /= fromJust (lookup a to)) (from \\ missing from to)
+        diffs = map (\(a,v) ->
+            (a, filterValues (fromJust $ lookup a to) v)) differ
+
+missing :: (Eq a, Eq b) => [(a, [b])] -> [(a, [b])] -> [(a, [b])]
+missing from to = filter (\(a, _) -> isNothing $ lookup a to) from
+
+filterValues :: (Eq a) => [a] -> [a] -> [a]
+filterValues from to = filter (`notElem` from) to
+
+toAL :: [LDIF] -> [(DN, LDIFRecord)]
+toAL = toAssocList
+
+testA = [(1, [1,2,3]), (2,[2]), (3, [3,4,5])]
+testB = [(1, [1,2]), (2,[2])]
+

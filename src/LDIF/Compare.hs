b43bbe62f8efa357f3dff92c42e2730dcddb097c
Jonas Juselius <jonas.juselius@uit.no>
2014-09-15 15:25:44 +0200

    Use type alias for (Attr, [Val]).

--- src/LDIF/Compare.hs
+++ src/LDIF/Compare.hs
@@ -2,7 +2,7 @@
 
     <jonas.juselius@uit.no> 2014
 -}
-{-# LANGUAGE OverloadedStrings, LiberalTypeSynonyms #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module LDIF.Compare (
       filterEqDN
@@ -15,10 +15,7 @@ import LDIF.Utils
 import Data.List
 import Data.Maybe
 
-type Foo = forall a b. (Eq a, Eq b) => (a, b)
-
-foo :: Foo -> Foo
-foo x = x
+type AttrVals a b = [(a, [b])]
 
 filterEqDN :: [LDIF] -> [LDIF] -> [LDIF]
 filterEqDN from to =
@@ -29,7 +26,7 @@ filterEq from to =
     map LDIF $ filter (\(dn, rec) ->
         maybe False (== rec) $ lookup dn (toAL to)) (toAL from)
 
-filterAttrs :: (Eq a, Eq b) => [(a, [b])] -> [(a, [b])] -> [(a, [b])]
+filterAttrs :: (Eq a, Eq b) => AttrVals a b -> AttrVals a b -> AttrVals a b
 filterAttrs from to = missing from to ++ diffs
     where
         differ = filter (\(a, v) ->
@@ -37,7 +34,7 @@ filterAttrs from to = missing from to ++ diffs
         diffs = map (\(a,v) ->
             (a, filterValues (fromJust $ lookup a to) v)) differ
 
-missing :: (Eq a, Eq b) => [(a, [b])] -> [(a, [b])] -> [(a, [b])]
+missing :: (Eq a, Eq b) => AttrVals a b -> AttrVals a b -> AttrVals a b
 missing from to = filter (\(a, _) -> isNothing $ lookup a to) from
 
 filterValues :: (Eq a) => [a] -> [a] -> [a]
@@ -46,6 +43,6 @@ filterValues from = filter (`notElem` from)
 toAL :: [LDIF] -> [(DN, LDIFRecord)]
 toAL = toAssocList
 
-testA = [(1, [1,2,3]), (2,[2]), (3, [3,4,5])]
-testB = [(1, [1,2]), (2,[2])]
+testA = [(1, [1,2,3]), (2,[2]), (3, [3,4,5])] :: AttrVals Int Int
+testB = [(1, [1,2]), (2,[2])] :: AttrVals Int Int
 

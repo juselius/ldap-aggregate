f7fa4a84086df0ebee7219a131f04b7cb5b5415a
Jonas Juselius <jonas.juselius@uit.no>
2014-09-24 14:26:13 +0200

    Added toplevel LDIF module for easy import.

---
+++ src/LDIF.hs
@@ -0,0 +1,11 @@
+module LDIF (
+      module LDIF.Types
+    , module LDIF.Parser
+    , module LDIF.Diff
+    , module LDIF.Utils
+) where
+
+import LDIF.Types
+import LDIF.Parser
+import LDIF.Diff
+import LDIF.Utils

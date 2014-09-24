4f61c4c0cfb9ad7895041ecaca698e22d9aaefd1
Jonas Juselius <jonas.juselius@uit.no>
2014-09-24 14:30:21 +0200

    Compute difference LDIF (LDAPMod) between LDAPEntries.

--- src/LDIF/Diff.hs
+++ src/LDIF/Diff.hs
@@ -33,39 +33,54 @@ module LDIF.Diff (
 ) where
 
 import LDIF.Types
+import LDAP.Modify (list2ldm)
 import Data.Maybe
-import Data.List (foldl')
 import Control.Arrow (second)
+
+-- | Calculate difference between two LDAP entries
+diffRecord :: LDIFRecord -> LDIFRecord -> LDIFRecord
+diffRecord r1 r2 = diffAttrs (getEntry r1) (getEntry r2)
+    where
+        getEntry = leattrs . fromJust . record2entry []
 
 -- | Calculate Change LDIF between two LDIF contents.
 -- If there is not difference the empty change list is returned.
-diffLDIF :: [LDIFContents] -> [LDIFContents] -> [LDIF]
-diffLDIF c1 c2 = adds  ++ deletes ++ changes
+diffLDIF :: [LDAPEntry] -> [LDAPEntry] -> [LDIF]
+diffLDIF r1 r2 = adds ++ deletes ++ changes
     where
-        adds = map content2add $ filter (not . isEntryIn) l2
+        adds = map e2add $ filter (not . isEntryIn l1) l2
+        (changes, deletes) = foldr processEntry ([], []) l1
+        [l1, l2] = map (map (\(LDAPEntry dn av) -> (dn, av))) [r1, r2]
+        e2add x = LDIF $ second (LDIFAdd . list2ldm LdapModAdd) x
+        processEntry (dn, e1) (cx, dx) = maybe delRec chRec (lookup dn l2)
             where
-                isEntryIn (dn, _) = isJust $ lookup dn l1
-                content2add x = LDIF $ second LDIFAdd x
-        (changes, deletes) = foldl' processEntry ([], []) l1
-        processEntry (cx, dx) (dn, e1) = maybe delRec chRec (lookup dn l2)
-            where
-                chRec e2 = (diffToLdif e2 ++ cx, dx)
+                chRec e2 = (diffToLdif e2:cx, dx)
                 delRec = (cx, LDIF (dn, LDIFDelete):dx)
-                diffToLdif e2 = map (\x -> LDIF (dn, x)) $ diffAttrs e1 e2
-        [l1, l2] = map toAList [c1, c2]
+                diffToLdif e2 = LDIF (dn, diffAttrs e1 e2)
 
--- | Calculate difference between two LDIF Records
-diffRecord :: LDIFRecord -> LDIFRecord -> [LDIFRecord]
-diffRecord LDIFDelete _ = []
-diffRecord _ LDIFDelete = []
-diffRecord r1 r2 = diffAttrs (ldifRecord r1) (ldifRecord r2)
-
-diffAttrs :: [AttrSpec] -> [AttrSpec] -> [LDIFRecord]
-diffAttrs r1 r2 = delMods ++ addMods
+diffAttrs :: [AttrSpec] -> [AttrSpec] -> LDIFRecord
+diffAttrs r1 r2 = LDIFChange $ adds ++ deletes ++ changes
     where
-        addMods = map LDIFAdd addVals
-        delMods = map (LDIFChange LdapModDelete) delVals
-        addVals = []
-        delVals = []
-        --addVals = filter (\x -> not $ elem x (coAttrVals r1)) (coAttrVals r2) :: [AttrValue]
-        --delVals = filter (\x -> not $ elem x (coAttrVals r2)) (coAttrVals r1) :: [AttrValue]
+        adds = map a2add $ filter (not . isEntryIn r1) r2
+        a2add (a, v) = LDAPMod LdapModAdd a v
+        (changes, deletes) = foldr processEntry ([], []) r1
+        processEntry (a, v) (cx, dx) = maybe delAttr chAttr (lookup a r2)
+            where
+                delAttr   = (cx, LDAPMod LdapModDelete a v:dx)
+                chAttr v' =  (add' ++ cx, del' ++ dx)
+                    where
+                        (adds', dels) = diffValues v v'
+                        add' = map (\x -> LDAPMod LdapModAdd a [x]) adds'
+                        del' = map (\x -> LDAPMod LdapModDelete a [x]) dels
+
+diffValues :: [Value] -> [Value] -> ([Value], [Value])
+diffValues r1 r2 = (sift r1 r2, sift r2 r1)
+
+sift :: Eq a => [a] -> [a] -> [a]
+sift x = filter (not . flip elem x)
+
+isEntryIn :: Eq a => [(a, b)] -> (a, b) -> Bool
+isEntryIn l (e, _) = isJust $ lookup e l
+
+--testAV1 = [("foo", ["foo1", "bar1"]), ("bar", ["foo2", "bar2"])]
+--testAV2 = [("foo2", ["foo2", "bar2"])]

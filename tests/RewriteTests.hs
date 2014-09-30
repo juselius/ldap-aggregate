551b89732a944c773f4770aaa2bddf1bc91cef19
Jonas Juselius <jonas.juselius@uit.no>
2014-09-30 10:18:25 +0200

    Updated rewriting tests.
    Tests rewriting DN:s, attribute filtering and attribute rewrites.

--- tests/RewriteTests.hs
+++ tests/RewriteTests.hs
@@ -6,22 +6,43 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import LDAPRelay.Rewrite
 import TestData
-import LDIF.Utils (bimap1)
+import LDIF
+
+import Debug.Trace
 
 rewriteTests :: TestTree
-rewriteTests = testGroup "Rewrite unit tests" [
-      testCase "rewriteDN" $ test1 @?= True
-    , testCase "rewriteAttrList" $ test2 @?= True
-    , testCase "fitlerAttrs" $ test3 @?= True
+rewriteTests = testGroup "LDAPRewrite" [
+      testCase "rewrite dn" $ testRewriteDN @?= True
+    , testCase "rewrite attrs" $ testRewriteAttrs @?= True
+    , testCase "filter attrs" $ testFilterAttrs @?= True
     ]
 
-test1 = newDN == genLdif "dc=oof,dc=com" ldifDef1
-test2 = False
-test3 = False
+testRewriteDN = newDN == genLdif "dc=oof,dc=com" ldifDef1
+    where
+        newDN = rewriteDN [("foo", "oof"), ("com", "moc")] ldif1
 
-newDN = rewriteDN [("foo", "oof"), ("com", "moc")] ldif1
+testRewriteAttrs = rewritten == ldiff
+    where
+        rewritten = rewriteAttrs attrs ldif1
+        attrs = [
+            ("A", ("^A1$", "a1"))
+            , ("B", ("B1", "hubba"))
+            ]
+        ldiff = uncurry genLdif ("dc=foo,dc=com", [
+              ("A", ["a1", "A2"])
+            , ("B", ["hubba"])
+            ])
 
-l1 = ("dc=foo,dc=com", ldifDef1)
-l2 = ("dc=bar,dc=org", ldifDef2)
+testFilterAttrs = filtered == ldiff
+    where
+        filtered = filterEntry attrs ldif1
+        attrs = (".*", ["B"])
+        ldiff = uncurry genLdif ("dc=foo,dc=com", [
+              ("A", ["A1", "A2"])
+            ])
 
 (ldif1, ldif2) = bimap1 (uncurry genLdif) (l1, l2)
+    where
+        l1 = ("dc=foo,dc=com", ldifDef1)
+        l2 = ("dc=bar,dc=org", ldifDef2)
+
--- tests/TestData.hs
+++ tests/TestData.hs
@@ -18,35 +18,6 @@ genLdif' dn av = BS.pack $ "dn: " ++ dn ++ "\n" ++ attrs
 genLdif :: String -> [AttrSpec] -> LDIF
 genLdif dn av = LDIF (dn, LDIFEntry $ LDAPEntry dn av)
 
-
-ldifDef1 :: [AttrSpec]
-ldifDef1 = [
-      ("A", ["A1", "A2"])
-    , ("B", ["B1"])
-    ]
-
-ldifDef2 :: [AttrSpec]
-ldifDef2 = [
-      ("A", ["A1"])
-    ]
-
-ldiff1 :: [LDIF]
-ldiff1 = [
-      LDIF ("B", LDIFAdd [LDAPMod LdapModAdd "A" ["A1"]])
-    , LDIF ("A", LDIFDelete)
-    ]
-
-ldiff2 :: [LDIF]
-ldiff2 = [
-      LDIF ("A", LDIFAdd [ LDAPMod LdapModAdd "A" ["A1", "A2"]
-                         , LDAPMod LdapModAdd "B" ["B1"]
-                         ]
-           )
-    , LDIF ("B", LDIFDelete)
-    ]
-
-sampleLdif = sample' (arbitrary :: Gen LdifEntryStr)
-
 newtype LdifEntryStr = LdifEntryStr { entryStr :: String } deriving (Show)
 
 instance Arbitrary LdifEntryStr where
@@ -108,3 +79,33 @@ genSafeString = (:) <$> genSafeChar <*> listOf genSafeAlpha
 
 genSafeString1 :: Gen String
 genSafeString1 = (:) <$> genSafeChar <*> listOf1 genSafeAlpha
+
+
+ldifDef1 :: [AttrSpec]
+ldifDef1 = [
+      ("A", ["A1", "A2"])
+    , ("B", ["B1"])
+    ]
+
+ldifDef2 :: [AttrSpec]
+ldifDef2 = [
+      ("A", ["A1"])
+    ]
+
+ldiff1 :: [LDIF]
+ldiff1 = [
+      LDIF ("B", LDIFAdd [LDAPMod LdapModAdd "A" ["A1"]])
+    , LDIF ("A", LDIFDelete)
+    ]
+
+ldiff2 :: [LDIF]
+ldiff2 = [
+      LDIF ("A", LDIFAdd [ LDAPMod LdapModAdd "A" ["A1", "A2"]
+                         , LDAPMod LdapModAdd "B" ["B1"]
+                         ]
+           )
+    , LDIF ("B", LDIFDelete)
+    ]
+
+sampleLdif = sample' (arbitrary :: Gen LdifEntryStr)
+

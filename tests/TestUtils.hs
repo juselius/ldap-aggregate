160a4380dd66afecbb6af9ba6cf52df303a7afcf
Jonas Juselius <jonas.juselius@uit.no>
2014-10-03 16:04:35 +0200

    Update to reflect refactored LDAPRelay.

--- tests/TestUtils.hs
+++ tests/TestUtils.hs
@@ -45,7 +45,7 @@ populateSource ldap = do
     ltree <- withFile "./ldif/source.tree.ldif" ReadMode BS.hGetContents
     lpops <- withFile "./ldif/source.populate.ldif" ReadMode BS.hGetContents
     let commit x = mapM_ (\(dn, attrs) ->
-            ldapAdd ldap dn attrs) (ldapStr2LdapMod x)
+            ldapAdd ldap dn attrs) (ldifStr2LdapAdd x)
     commit ltree
     commit lpops
 

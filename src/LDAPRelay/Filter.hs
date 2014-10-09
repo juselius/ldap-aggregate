cf25595224157f41068b03d914cb480b1a9833b3
Jonas Juselius <jonas.juselius@uit.no>
2014-10-09 15:29:26 +0200

    Implemented DN and Attribute filtering for the general cases.

--- src/LDAPRelay/Filter.hs
+++ src/LDAPRelay/Filter.hs
@@ -6,51 +6,77 @@ module LDAPRelay.Filter (
       filterLdif
     , filterDN
     , filterEntries
-    , filterEntry
+    , makeDnFilters
+    , makeAttrFilters
 ) where
 
 import LDIF
 import LDAPRelay.Types
 import Text.Regex.Posix
 import Control.Arrow (second)
-import Data.Maybe (maybe)
+import Data.List (partition, foldl')
+
+makeDnFilters :: [String] -> [Filter]
+makeDnFilters = map FilterDn
+
+makeAttrFilters :: [[String]] -> [Filter]
+makeAttrFilters = map toFilter
+    where
+        toFilter (a:[]) = FilterAttr a
+        toFilter (dn:a:[]) = FilterDnAttr dn a
+        toFilter (dn:a:v:[]) = FilterDnVal dn a v
+        toFilter x = error $ "Invalid filter spec: " ++ show x
+
 
 filterLdif :: [Filter] -> [LDIF] -> [LDIF]
-filterLdif fs ldif = map (filterLDIF fs) ldif'
+filterLdif fs ldif = filterEntries fs ldif'
     where
-        ldif' = filterDN fs ldif
+        ldif' = filterDN fs' ldif
+        fs' = fst $ partition isDnFilter fs
 
 filterDN :: [Filter] -> [LDIF] -> [LDIF]
-filterDN fs ldif = filter (\(dn, _) -> any (dnFilter dn) fs) ldif
+filterDN fs = filter (\(dn, _) -> any (dnFilter dn) fs)
     where
         dnFilter dn' (FilterDn f) = f =~ dn'
         dnFilter _ _ = False
 
-filterLDIF :: [Filter] -> LDIF -> LDIF
-filterLDIF fs l@(dn, x) = case x of
-    LDIFEntry (LDAPEntry dn' av) ->
-        (dn, LDIFEntry (LDAPEntry dn' (filterAV av)))
-    {-LDIFAdd lm -> (dn, (LDIFAdd filterLM lm))-}
-    {-LDIFChange lm -> (dn, (LDIFChange filterLM lm))-}
-    LDIFDelete -> l
+filterEntries :: [Filter] -> [LDIF] -> [LDIF]
+filterEntries fs = map (filterLDIF fs')
     where
-        filterAV av = filter (\x -> any (runAvFilter x) fs) av
-        {-filterLM lm = filter (\x -> any (runAvFilter x) fs) lm-}
-        runAvFilter (a, v) (FilterDnAttr dn' at) = False
-        runAvFilter (a, v) (FilterDnAttrVal dn' at val) = False
-        runAvFilter (a, v) (FilterAttr at) = False
-        runAvFilter _ (FilterDn _) = False
+        fs' = snd $ partition isDnFilter fs
 
+filterLDIF :: [Filter] -> LDIF -> LDIF
+filterLDIF fs l@(dn, _) = second (liftLdif (runAttrFilters dn fs)) l
 
-filterEntries :: (DN, [Attribute]) -> [LDIF] -> [LDIF]
-filterEntries spec ldif = map (filterEntry spec) ldif
+runAttrFilters :: DN -> [Filter] -> [AttrSpec] -> [AttrSpec]
+runAttrFilters dn fs av = filter (\x -> any (matchAvFilter dn x) fs') av'
+    where
+        av' = filter (null . snd) $ map (filterValues dn fs'') av
+        (fs'', fs') = partition isValueFilter fs
+        isValueFilter (FilterDnVal {}) = True
+        isValueFilter _ = False
 
-filterEntry :: (DN, [Attribute]) -> LDIF -> LDIF
-filterEntry (dnFilter, attrs) x@(dn, _) =
-    if dn =~ dnFilter
-    then second (liftLdif (filterAttrs attrs)) x
-    else x
+matchAvFilter :: DN -> AttrSpec -> Filter -> Bool
+matchAvFilter dn (a, _) f = case f of
+    FilterAttr atf -> a =~ atf
+    FilterDnAttr dnf atf -> dn =~ dnf && a =~ atf
+    _ -> False
 
-filterAttrs :: [Attribute] -> [AttrSpec] -> [AttrSpec]
-filterAttrs attrl = filter (not . flip elem attrl . fst)
+filterValues :: DN -> [Filter] -> AttrSpec -> AttrSpec
+filterValues dn f as = foldl' applyFilter as f
+    where
+        applyFilter av (FilterDnVal dnf atf valf) =
+            if dn =~ dnf
+            then filterAV av
+            else av
+            where
+                filterAV e@(a, v) =
+                    if a =~ atf
+                    then (a, filter (=~ valf) v)
+                    else e
+        applyFilter acc _ = acc
+
+isDnFilter :: Filter -> Bool
+isDnFilter (FilterDn _) = True
+isDnFilter _ = False
 
--- src/LDAPRelay/Types.hs
+++ src/LDAPRelay/Types.hs
@@ -13,10 +13,10 @@ type RegexStr = String
 type FromTo = (RegexStr, RegexStr)
 
 data Filter =
-      FilterDn        { dnF   :: RegexStr }
-    | FilterDnAttr    { dnF   :: RegexStr, attrF:: String }
-    | FilterDnAttrVal { dnF   :: RegexStr, attrF:: String, valF :: String }
-    | FilterAttr      { attrF :: String }
+      FilterDn     { dnF   :: RegexStr }
+    | FilterAttr   { attrF :: RegexStr }
+    | FilterDnAttr { dnF   :: RegexStr, attrF:: RegexStr }
+    | FilterDnVal  { dnF   :: RegexStr, attrF:: RegexStr, valF :: RegexStr }
 
 data Rewrite =
       RewriteDn   { rwSpec   :: FromTo }

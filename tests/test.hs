453d9c5fe108c5f7649f7227bd8fc4c3ef90981e
Jonas Juselius <jonas.juselius@uit.no>
2015-03-26 10:59:51 +0100

    Save old work before refactor.
    Forgot what this was about.

--- tests/test.hs
+++ tests/test.hs
@@ -1,9 +1,18 @@
 import Test.Tasty
 import LdifTests
-import RewriteTests
+import LDIF
+import TestData
+--import RewriteTests
 
 main :: IO ()
-main = defaultMain tests
+main = do
+    let ldiff = genLdif "dc=foo,dc=com"
+            [ ("A", ["A1"])
+            , ("B", ["B1"])
+            , ("C", ["C1"])
+            ]
+    putStr $ showLdif ldiff
+    defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [ldifTests, rewriteTests]
+tests = testGroup "Tests" [ldifTests] --, rewriteTests]

b47c33bbd2b9674b04041711a1fee048e2acfd21
Jonas Juselius <jonas.juselius@uit.no>
2015-04-09 11:21:04 +0200

    Add rewrite test and monoid tests.

--- tests/EditTests.hs
+++ tests/EditTests.hs
@@ -5,12 +5,13 @@ module EditTests (
 
 import Test.Tasty
 import Test.Tasty.HUnit
+import Data.Monoid
 import Editor
 import qualified Data.HashMap.Lazy as HM
 import qualified Data.HashSet as HS
 import qualified Data.Text as T
 
-import Debug.Trace
+-- import Debug.Trace
 
 editTests :: TestTree
 editTests = testGroup "Editor" [
@@ -20,13 +21,28 @@ editTests = testGroup "Editor" [
     , testCase "rewrite 0" $ testR0 @?= True
     , testCase "rewrite 1" $ testR1 @?= True
     , testCase "rewrite 2" $ testR2 @?= True
+    , testCase "rewrite 3" $ testR3 @?= True
+    , testCase "monoid 0" $ testM0 @?= True
+    , testCase "monoid 1" $ testM1 @?= True
     ]
+
+ldif :: HM.HashMap T.Text (HM.HashMap T.Text (HS.HashSet T.Text))
+ldif = HM.fromList [
+      ("A", HM.fromList [
+          ("a1", HS.fromList ["a1-1", "a1-2"])
+        , ("a2", HS.fromList ["a2-1", "a2-2"])
+        ])
+    , ("B", HM.fromList [
+          ("b1", HS.fromList ["b1-1","b1-2"])
+        , ("b2", HS.fromList ["b2-1","b2-2"])
+        ]
+    )]
 
 testF0 :: Bool
 testF0 = edit f ldif == ref
     where
-        f :: [Criterion T.Text]
-        f = [Break "A"]
+        f :: Rule T.Text
+        f = Delete "A" Done
         ref = HM.fromList [
             ("B", HM.fromList [
               ("b1", HS.fromList ["b1-1","b1-2"])
@@ -37,8 +53,8 @@ testF0 = edit f ldif == ref
 testF1 :: Bool
 testF1 = edit f ldif == ref
     where
-        f :: [Criterion T.Text]
-        f = [Cont "A", Break "a1"]
+        f :: Rule T.Text
+        f = Delete "A" $ Delete "a1" Done
         ref = HM.fromList [
               ("A", HM.fromList [
                   ("a2", HS.fromList ["a2-1", "a2-2"])
@@ -52,8 +68,8 @@ testF1 = edit f ldif == ref
 testF2 :: Bool
 testF2 = edit f ldif == ref
     where
-        f :: [Criterion T.Text]
-        f = [Cont "A", Cont "a1", Break "a1-1"]
+        f :: Rule T.Text
+        f = Delete "A" $ Delete "a1" $ Delete "a1-1" Done
         ref = HM.fromList [
               ("A", HM.fromList [
                   ("a1", HS.fromList ["a1-2"])
@@ -68,8 +84,8 @@ testF2 = edit f ldif == ref
 testR0 :: Bool
 testR0 = edit rw ldif == ref
     where
-        rw :: [Criterion FromTo]
-        rw = [Break ("A", "C")]
+        rw :: Rule T.Text
+        rw = Subst "A" "C" Done
         ref = HM.fromList [
               ("C", HM.fromList [
                   ("a1", HS.fromList ["a1-1", "a1-2"])
@@ -84,8 +100,8 @@ testR0 = edit rw ldif == ref
 testR1 :: Bool
 testR1 = edit rw ldif == ref
     where
-        rw :: [Criterion FromTo]
-        rw = [Cont ("A", "C"), Break ("a([0-9])", "c\\1")]
+        rw :: Rule T.Text
+        rw = Subst "A" "C" $ Subst "a([0-9])" "c\\1" Done
         ref = HM.fromList [
               ("C", HM.fromList [
                   ("c1", HS.fromList ["a1-1", "a1-2"])
@@ -98,12 +114,12 @@ testR1 = edit rw ldif == ref
             )]
 
 testR2 :: Bool
-testR2 = trace (show (foldl (flip edit) ldif rw)) foldl (flip edit) ldif rw == ref
+testR2 = foldl (flip edit) ldif rw == ref
     where
-        rw :: [[Criterion FromTo]]
+        rw :: [Rule T.Text]
         rw = [
-              [Cont ("A", "C"), Cont ("a([0-9])", "c\\1"), Break ("a1-", "c1-")]
-            , [Cont ("(.*)", "\\1"), Cont ("(.*)", "\\1"), Break ("a2-", "c2-")]
+              Subst "A" "C" $ Subst "a([0-9])" "c\\1" $ Subst "a1-" "c1-" Done
+            , Subst "(.*)" "\\1" $ Subst "(.*)" "\\1" $ Subst "a2-" "c2-" Done
             ]
         ref = HM.fromList [
               ("C", HM.fromList [
@@ -116,16 +132,24 @@ testR2 = trace (show (foldl (flip edit) ldif rw)) foldl (flip edit) ldif rw == r
                 ]
             )]
 
-ldif :: HM.HashMap T.Text (HM.HashMap T.Text (HS.HashSet T.Text))
-ldif = HM.fromList [
-      ("A", HM.fromList [
-          ("a1", HS.fromList ["a1-1", "a1-2"])
-        , ("a2", HS.fromList ["a2-1", "a2-2"])
-        ])
-    , ("B", HM.fromList [
-          ("b1", HS.fromList ["b1-1","b1-2"])
-        , ("b2", HS.fromList ["b2-1","b2-2"])
-        ]
-    )]
+testR3 :: Bool
+testR3 = foldl (flip edit) ldif rw == ref
+    where
+        rw :: [Rule T.Text]
+        rw = [
+              Subst "A" "" $ Subst "(.*)" "\\1" Done
+            , Subst "(.*)" "\\1" $ Subst "(.*)" "\\1" $ Subst ".*" "" Done
+            ]
+        ref = HM.empty
 
+testM0 :: Bool
+testM0 = Done <> m <> mempty == m
+    where m = Delete T.empty Done
+
+testM1 :: Bool
+testM1 = m0 <> m1 <> mempty == ref
+    where
+        m0  = Delete 0 $ Delete 1 Done :: Rule Int
+        m1  = Delete 2 $ Delete 3 Done :: Rule Int
+        ref = Delete 0 $ Delete 1 $ Delete 2 $ Delete 3 Done :: Rule Int
 

02c37f130384375c8ea5704c1b62edfa01538e15
Jonas Juselius <jonas.juselius@uit.no>
2015-04-09 10:46:08 +0200

    Refactor and hlint Rules.

--- src/Editor/Rules.hs
+++ src/Editor/Rules.hs
@@ -2,7 +2,6 @@
 -- <jonas.juselius@uit.no> 2014
 --
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ViewPatterns #-}
 module Editor.Rules (
       Rule(..)
     , IgnoreRule(..)
@@ -15,8 +14,6 @@ import Data.Maybe
 import Data.Monoid
 import Control.Applicative
 import Control.Monad
-import Text.Regex
-import Text.Regex.TDFA
 import Editor.Edit
 import qualified Data.Text as T
 
@@ -24,45 +21,46 @@ newtype IgnoreRule  = IgnoreRule  (Rule T.Text) deriving (Show)
 newtype RewriteRule = RewriteRule (Rule T.Text) deriving (Show)
 newtype InsertRule  = InsertRule  (Rule T.Text) deriving (Show)
 
+type RuleT = Rule T.Text
+type RuleF a = Maybe a -> Rule T.Text
+
 instance FromJSON IgnoreRule where
     parseJSON (Object o) = do
-        dn    <- o .:? "dn"
-        attr  <- o .:? "attr"
-        value <- o .:? "value"
-        let
-            dnR    = maybe every final dn
-            valueR = maybe Done final value
-            attrR  = if isNothing value
-                then maybe Done final attr
-                else maybe every final attr
-        return . IgnoreRule $ dnR `mappend` attrR `mappend` valueR
-            where
-                every = Delete ".*" Done
-                final x = Delete x Done
+        dn    <- liftM dnR   $ o .:? "dn"
+        attr  <- liftM attrR $ o .:? "attr"
+        value <- liftM valR  $ o .:? "value"
+        return . IgnoreRule $ dn `mappend` attr `mappend` value
+        where
+            (dnR, attrR, valR) = mkRs glob final
+            glob = Delete ".*" Done
+            final x = Delete x Done
     parseJSON _ = mzero
 
 instance FromJSON RewriteRule where
     parseJSON (Object o) = do
-        dn    <- o `getFromTo` "dn"
-        attr  <- o `getFromTo` "attr"
-        value <- o `getFromTo` "value"
-        let
-            dnR    = maybe every final dn
-            valueR = maybe Done final value
-            attrR  = if isNothing value
-                then maybe Done final attr
-                else maybe every final attr
-        return . RewriteRule $ dnR `mappend` attrR `mappend` valueR
+        dn    <- liftM dnR   $ o `getFromTo` "dn"
+        attr  <- liftM attrR $ o `getFromTo` "attr"
+        value <- liftM valR  $ o `getFromTo` "value"
+        return . RewriteRule $ dn `mappend` attr `mappend` value
         where
-            every = Subst "(.*)" "\\1" Done
+            (dnR, attrR, valR) = mkRs glob final
+            glob = Subst "(.*)" "\\1" Done
             final (f, t) = Subst f t Done
             getFromTo x s = fmap parseFromTo (x .:? s)
-            parseFromTo :: Maybe Value -> Maybe (T.Text, T.Text)
-            parseFromTo (Just (Object x)) =
-                flip parseMaybe x $ \y -> (,)
-                    <$> y .: "from"
-                    <*> y .: "to"
-            parseFromTo _ = Nothing
     parseJSON _ = mzero
 
+parseFromTo :: Maybe Value -> Maybe (T.Text, T.Text)
+parseFromTo (Just (Object x)) =
+    flip parseMaybe x $ \y -> (,)
+        <$> y .: "from"
+        <*> y .: "to"
+parseFromTo _ = Nothing
 
+mkRs :: RuleT -> (a -> RuleT) -> (RuleF a, RuleF a, RuleF a)
+mkRs e f = (
+      maybe e f
+    , maybe Done f
+    , \x -> if isNothing x
+              then maybe Done f x
+              else maybe e f x
+    )

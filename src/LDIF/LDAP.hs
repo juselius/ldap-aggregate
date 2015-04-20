91768e243c16420a0cfc59eae4c0c88775c1110d
Jonas Juselius <jonas.juselius@uit.no>
2015-04-20 13:04:31 +0200

    Update LDAP to use new types.

--- src/LDIF/LDAP.hs
+++ src/LDIF/LDAP.hs
@@ -15,22 +15,21 @@ import qualified Data.HashMap.Lazy as HM
 import qualified Data.HashSet as HS
 import qualified Data.Text as T
 
-ldapToLdif :: [LDAPEntry] -> LDIF
+ldapToLdif :: [LDAPEntry] -> LDIFEntries
 ldapToLdif x = HM.fromList $ map toll x
     where
         toll (LDAPEntry dn av) = let dn' = T.pack dn in
-            (dn', LDIFAdd dn' (HM.fromList $ map toat av))
-        toat (a, v) =
-            (T.pack a, HS.fromList (map T.pack v))
+            (dn', LDIFRecord dn' (LDIFAttrs . HM.fromList $ map toat av))
+        toat (a, v) = (T.pack a, HS.fromList (map T.pack v))
 
-recordToLdapAdd :: LdifAttrs T.Text -> [LDAPMod]
-recordToLdapAdd la = map f $ HM.toList la
+recordToLdapAdd :: LDIFAttrs T.Text -> [LDAPMod]
+recordToLdapAdd la = map f $ HM.toList (toHM la)
     where
         f (a, v) = LDAPMod LdapModAdd (T.unpack a) $ vl v
         vl v = map T.unpack (HS.toList v)
 
-recordToLdapMod :: LdifAttrs (LDAPModOp, T.Text) -> [LDAPMod]
-recordToLdapMod lm = concatMap f $ HM.toList lm
+recordToLdapMod :: LDIFAttrs (LDAPModOp, T.Text) -> [LDAPMod]
+recordToLdapMod lm = concatMap f $ HM.toList (toHM lm)
     where
         f (a, v) = map (\(m, x) -> LDAPMod m (T.unpack a) x) $ vl v
         vl v = map (\(m, x) -> (m, [T.unpack x])) (HS.toList v)

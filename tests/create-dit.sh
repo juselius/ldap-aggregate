1a3cda05124dc4373dcb602420792a53c8d03339
Jonas Juselius <jonas.juselius@uit.no>
2015-05-20 07:23:39 +0200

    Fix and simplify create-dit.sh.

--- tests/create-dit.sh
+++ tests/create-dit.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo -n "bindpw: "
-read pw
-
-ldapadd -H ldap://localhost -D cn=admin,dc=source -w $pw\
-    -f ./ldif/source.bootstrap.ldif
-ldapadd -H ldap://localhost -D cn=admin,dc=target -w $pw\
-    -f ./ldif/target.bootstrap.ldif
+ldapadd -H ldap://localhost -D cn=admin,dc=source -w secret\
+    -f ./ldif/source.dit.ldif
+ldapadd -H ldap://localhost -D cn=admin,dc=target -w secret\
+    -f ./ldif/target.dit.ldif
